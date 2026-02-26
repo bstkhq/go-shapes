8db93b3a27031f091d17e18f4aef97bb7bf038f2
tinne <tinne26k@gmail.com>
2026-02-26 17:36:06 +0100

    Remove Apply*Shadow methods They are quirky to use and haven't been used in practice once the standard blur methods took over. Let's reduce the noise.

--- renderer_apply_test.go
+++ renderer_apply_test.go
@@ -1,7 +1,6 @@
 package shapes
 
 import (
-	"image"
 	"image/color"
 	"math"
 	"testing"
@@ -120,93 +119,6 @@ func TestApplyOutline(t *testing.T) {
 		}
 	})
 	app.Images = append(app.Images, app.Renderer.NewCircle(float64(radius)))
-	if err := ebiten.RunGame(app); err != nil {
-		t.Fatal(err)
-	}
-}
-
-// go test -run ^TestApplyHardShadow$ . -count 1
-func TestApplyHardShadow(t *testing.T) {
-	radius := float32(64.0)
-	app := NewTestApp(func(canvas *ebiten.Image, ctx TestAppCtx) {
-		canvas.Fill(color.Black)
-
-		lx, ly := ctx.LeftClickF32()
-		ctx.Renderer.SetColor(color.RGBA{0, 128, 128, 128})
-		ctx.Renderer.ApplyHardShadow(canvas, ctx.Images[0], lx-radius, ly-radius, 0, 8.0, ClampLeft)
-		ctx.Renderer.SetColor(color.RGBA{255, 255, 255, 255})
-		ctx.Renderer.DrawCircle(canvas, lx, ly, radius)
-
-		rx, ry := ctx.RightClickF32()
-		ctx.Renderer.SetColor(color.RGBA{128, 128, 128, 128})
-		ctx.Renderer.ApplyHardShadow(canvas, ctx.Images[0], rx-radius, ry-radius, 8.0, 0.0, ClampNone)
-		ctx.Renderer.SetColor(color.RGBA{255, 255, 255, 255})
-		ctx.Renderer.DrawCircle(canvas, rx, ry, radius)
-	})
-	app.Images = append(app.Images, app.Renderer.NewCircle(float64(radius)))
-	if err := ebiten.RunGame(app); err != nil {
-		t.Fatal(err)
-	}
-}
-
-// go test -run ^TestApplyShadow$ . -count 1
-func TestApplyShadow(t *testing.T) {
-	app := NewTestApp(func(canvas *ebiten.Image, ctx TestAppCtx) {
-		canvas.Fill(color.Black)
-
-		lx, ly := ctx.LeftClickF32()
-		ctx.Renderer.SetColor(color.RGBA{0, 128, 128, 128})
-		ctx.Renderer.ApplyShadow(canvas, ctx.Images[0], lx, ly, 0, -16.0, 4.0, ClampBottom)
-		ctx.DrawAtF32(canvas, ctx.Images[0], lx, ly)
-
-		rx, ry := ctx.RightClickF32()
-		ctx.Renderer.SetColor(color.RGBA{128, 128, 128, 128})
-		ctx.Renderer.ApplyShadow(canvas, ctx.Images[0], rx, ry, -12.0, -12.0, 9.0, ClampBottom)
-		ctx.DrawAtF32(canvas, ctx.Images[0], rx, ry)
-
-		mx, my := min(lx, rx), max(ly, ry)
-		const MaxRadius = 16.0
-		r := float32(ctx.DistAnim(MaxRadius, 1.0))
-		ctx.Renderer.SetColorF32(1.0, 0.0, 0.0, 1.0)
-		ctx.Renderer.DrawCircle(canvas, mx+64, my+64, 64+r)
-		ctx.Renderer.SetColor(color.RGBA{0, 196, 196, 196})
-		ctx.Renderer.ApplyShadow(canvas, ctx.Images[1], mx, my, 0, 0, r, ClampNone)
-		ctx.DrawAtF32(canvas, ctx.Images[1], mx, my)
-
-		mx, my = max(lx, rx), min(ly, ry)
-		ctx.Renderer.SetColorF32(1.0, 1.0, 1.0, 1.0)
-		ctx.Renderer.ApplyBlur(canvas, ctx.Images[1], mx-32, my, r)
-		ctx.Renderer.ApplyShadow(canvas, ctx.Images[1], mx+32, my, 0, 0, r, ClampNone)
-	})
-	circle := app.Renderer.NewCircle(64.0)
-	circBounds := circle.Bounds()
-	halfCircle := circle.SubImage(image.Rect(0, 0, circBounds.Dx(), circBounds.Dy()/2)).(*ebiten.Image)
-	app.Images = append(app.Images, halfCircle, circle)
-	if err := ebiten.RunGame(app); err != nil {
-		t.Fatal(err)
-	}
-}
-
-// go test -run ^TestApplyZoomShadow$ . -count 1
-func TestApplyZoomShadow(t *testing.T) {
-	app := NewTestApp(func(canvas *ebiten.Image, ctx TestAppCtx) {
-		canvas.Fill(color.Black)
-
-		lx, ly := ctx.LeftClickF32()
-		ctx.Renderer.SetColor(color.RGBA{0, 128, 128, 128})
-		ctx.Renderer.ApplyZoomShadow(canvas, ctx.Images[0], lx, ly, 0, 0, 2.0, ClampNone)
-		ctx.DrawAtF32(canvas, ctx.Images[0], lx, ly)
-
-		rx, ry := ctx.RightClickF32()
-
-		ctx.Renderer.SetColor(color.RGBA{128, 0, 128, 128})
-		ctx.Renderer.ApplyZoomShadow(canvas, ctx.Images[1], rx, ry, 0, 0, 1.2, ClampBottom)
-		ctx.DrawAtF32(canvas, ctx.Images[1], rx, ry)
-	})
-	circle := app.Renderer.NewCircle(64.0)
-	circBounds := circle.Bounds()
-	halfCircle := circle.SubImage(image.Rect(0, 0, circBounds.Dx(), circBounds.Dy()/2)).(*ebiten.Image)
-	app.Images = append(app.Images, circle, halfCircle)
 	if err := ebiten.RunGame(app); err != nil {
 		t.Fatal(err)
 	}
